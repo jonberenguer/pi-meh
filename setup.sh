8b3e23be51d941850c84fced63aa1a464bdc2675
jberenguer <berenguer.jon@gmail.com>
2019-01-17 16:39:31 +0000

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BRANCH=next rpi-update
+rpi-update
 
 echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
 echo "dwc2" | sudo tee -a /etc/modules
@@ -9,8 +9,8 @@ echo "libcomposite" | sudo tee -a /etc/modules
 sed -i 's/exit/\/usr\/bin\/pi-meh_usb\nexit/' /etc/rc.local
 
 
-# 512mb storage image for mass storage
-dd if=/dev/zero of=~/pi-meh_usbdisk.img bs=1048576 count=512
+# 128mb storage image for mass storage
+dd if=/dev/zero of=/home/pi/pi-meh_usbdisk.img bs=1048576 count=128
 mkdosfs /home/pi/pi-meh_usbdisk.img
 
 cp pi-meh_usb /usr/bin/pi-meh_usb
@@ -20,4 +20,5 @@ cd hardpass-sendHID
 make && make install
 
 reboot
+
 
