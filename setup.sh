24e712a1bd3ea1e0e1cb70077060b3c6c2be286c
jberenguer <berenguer.jon@gmail.com>
2019-01-17 16:15:30 +0000

    added pi-meh_usb setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BRANCH=next rpi-update
+
+echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
+echo "dwc2" | sudo tee -a /etc/modules
+echo "libcomposite" | sudo tee -a /etc/modules
+
+sed -i 's/exit/\/usr\/bin\/pi-meh_usb\nexit/' /etc/rc.local
+
+
+# 512mb storage image for mass storage
+dd if=/dev/zero of=~/pi-meh_usbdisk.img bs=1048576 count=512
+mkdosfs /home/pi/pi-meh_usbdisk.img
+
+cp pi-meh_usb /usr/bin/pi-meh_usb
