fa8685daeac29e12cd24544ed4bc11e1200a7426
jberenguer <berenguer.jon@gmail.com>
2019-01-18 06:11:59 +0000

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -25,6 +25,8 @@ echo "libcomposite" | sudo tee -a /etc/modules
 dd if=/dev/zero of=/home/pi/pi-meh_usbdisk.img bs=1048576 count=128
 mkdosfs /home/pi/pi-meh_usbdisk.img
 
+cd /home/pi/pi-meh
+
 cp pi-meh_usb /usr/bin/pi-meh_usb
 
 cp pi-meh.service /etc/systemd/system/pi-meh.service
